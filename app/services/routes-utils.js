e6c70e3f830fa9740f587cb8ca97f4add0b9af20
Stephane <stephane@MBP-de-Stephane-2.lan>
2016-07-13 22:20:21 +0200

    login design

---
+++ app/services/routes-utils.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+    // routes:null,
+    // init() {
+    //     this._super(...arguments);
+    //     this.set('routes', []);
+    // }
+
+
+});
---
+++ tests/unit/services/routes-utils-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:routes-utils', 'Unit | Service | routes utils', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let service = this.subject();
+  assert.ok(service);
+});
